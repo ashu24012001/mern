7b2c0561f4d8de274e778c2b088eee5da467777c
Kumar <2102803@cognizant.com>
2022-11-03 16:33:59 +0530

    Addition of React app

---
+++ public/precache-manifest.11e8c731f4cf0480770c440292d85e25.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea3fda234f3168b011a2989f0798287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4b586f490c464a84d39",
+    "url": "/static/css/3.c4ff16f4.chunk.css"
+  },
+  {
+    "revision": "c7722c962ec5f4872949",
+    "url": "/static/css/4.c11346fe.chunk.css"
+  },
+  {
+    "revision": "cdea00c948039dd8d1c4",
+    "url": "/static/css/5.8cd69542.chunk.css"
+  },
+  {
+    "revision": "84c5d1537369ca42c840",
+    "url": "/static/css/6.1326459c.chunk.css"
+  },
+  {
+    "revision": "e1498b2a38fa89c3f6a6",
+    "url": "/static/css/7.295134cd.chunk.css"
+  },
+  {
+    "revision": "7382f1c575a0ec1d11a8",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "cb70d026bfbc86f7fd34",
+    "url": "/static/js/2.9a6cdbf8.chunk.js"
+  },
+  {
+    "revision": "d4b586f490c464a84d39",
+    "url": "/static/js/3.4f019cf8.chunk.js"
+  },
+  {
+    "revision": "c7722c962ec5f4872949",
+    "url": "/static/js/4.e4a2cac0.chunk.js"
+  },
+  {
+    "revision": "cdea00c948039dd8d1c4",
+    "url": "/static/js/5.a5d41080.chunk.js"
+  },
+  {
+    "revision": "84c5d1537369ca42c840",
+    "url": "/static/js/6.965a32c9.chunk.js"
+  },
+  {
+    "revision": "e1498b2a38fa89c3f6a6",
+    "url": "/static/js/7.f8285984.chunk.js"
+  },
+  {
+    "revision": "7382f1c575a0ec1d11a8",
+    "url": "/static/js/main.81a6352e.chunk.js"
+  },
+  {
+    "revision": "586cce7681fb1bbc7d2a",
+    "url": "/static/js/runtime-main.23fb8ffb.js"
+  }
+]);
